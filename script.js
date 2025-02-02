833b928bf99c3e6ae826a679404016c5b1a88dc9
syxm1 <hisyamprsetyo@gmail.com>
2025-02-02 16:52:28 +0700

    add blur animation on question reveal (unfinished on unblur)

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ let board = new Array(10).fill(null).map(() => new Array(10).fill('-'));
 let isShown = new Array(10).fill(null).map(() => new Array(10).fill(0));
 let isRevealed = new Array(words_num).fill(0);
 
-
 for (let i = 0; i < words_num; i++) {
     for (let j = 0; j < words[i][0].length; j++) {
         let x = words[i][3] === 'a' ? words[i][1] : words[i][1] + j;
@@ -60,6 +59,7 @@ for (let i = 0; i < board.length; i++) {
     }
 }
 
+let onQuestion = false;
 document.addEventListener("keydown", function(event) {
     let num = parseInt(event.key, 10) - 1;
 
@@ -82,12 +82,12 @@ document.addEventListener("keydown", function(event) {
                 element.appendChild(text);
 
                 let x = words[num][3] === 'a' 
-                        ? words[num][1]
-                        : words[num][1] + j;
+                ? words[num][1]
+                : words[num][1] + j;
                 let y = words[num][3] === 'a'
-                        ? words[num][2] + j
-                        : words[num][2];
-
+                ? words[num][2] + j
+                : words[num][2];
+                
                 if (isShown[x][y] === 0) {
                     element.classList.add('animate-entrance'); 
                     box.appendChild(element);
@@ -108,30 +108,21 @@ document.addEventListener("keydown", function(event) {
     if (event.ctrlKey && event.altKey && Number.isInteger(num) && num < words_num) {
         event.preventDefault();
 
-        for (let j = 0; j < words[num][0].length; j++) {
-
-            let id = words[num][3] === 'a' 
-                    ? ["item", words[num][1] + 1, "-", words[num][2] + j + 1].join('')
-                    : ["item", words[num][1] + j + 1, "-", words[num][2] + 1].join('');
-
-            let box = document.getElementById(id);
-            let text = document.createTextNode(words[num][0][j]);
-
-            let x = words[num][3] === 'a' 
-                    ? words[num][1]
-                    : words[num][1] + j;
-            let y = words[num][3] === 'a'
-                    ? words[num][2] + j
-                    : words[num][2];
-            
-            // screen blinked red
-        }     
     }
 
     // reveal question
-    if (event.ctrlKey && event.shiftKey && Number.isInteger(num) && num < words_num) {
-        event.preventDefault();
 
+    if (event.altKey && Number.isInteger(num) && num < words_num) {
+        event.preventDefault();
+        const content = document.getElementById('content');
         
+        if (onQuestion) {
+            content.classList.add('animate-unblur'); 
+            onQuestion = false;
+        }
+        else {
+            content.classList.add('animate-blur'); 
+            onQuestion = true;
+        }
     }
 });
